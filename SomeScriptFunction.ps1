43524729f3f1045139ebc37d0ba6be15c3bb97bf
pavel.vik@greycorbel.com <pavel.vik@greycorbel.com>
2024-12-10 13:51:37 +0100

    add scripts

---
+++ SomeScriptFunction.ps1
@@ -0,0 +1,23 @@
+function Get-FilesWithSizes {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Directory
+    )
+
+    # Initialize an array to store file information
+    $fileArray = @()
+
+    # Get all files from the directory and subdirectories
+    Get-ChildItem -Path $Directory -Recurse -File | ForEach-Object {
+        # Create a custom object with file path and size
+        $fileInfo = [PSCustomObject]@{
+            FilePath = $_.FullName
+            FileSize = $_.Length
+        }
+        # Add the object to the array
+        $fileArray += $fileInfo
+    }
+
+    # Return the array
+    return $fileArray
+}
