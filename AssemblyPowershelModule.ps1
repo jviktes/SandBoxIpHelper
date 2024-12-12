1e42b798fc7652f02c37ad81fb56a0022a751e47
pavel.vik@greycorbel.com <pavel.vik@greycorbel.com>
2024-12-12 14:39:54 +0100

    AssemblyPowershelModule for preparing files

---
+++ AssemblyPowershelModule.ps1
@@ -0,0 +1,19 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$RootDirectory,
+    #$env:GITHUB_WORKSPACE
+    [Parameter(Mandatory = $true)]
+    [string]$ModuleName
+    #SandBoxIpHelper
+)
+
+#Create folders:
+
+New-Item -Path "outputModule\" -ItemType Directory -Force
+New-Item -Path "outputModule\$($ModuleName)\" -ItemType Directory -Force
+New-Item -Path "outputModule\$($ModuleName)\Helpers" -ItemType Directory -Force
+
+#Copying required files:
+Get-ChildItem -Path (Join-Path $RootDirectory "Helpers") -Filter "*.cs" | Copy-Item -Destination (Join-Path $RootDirectory "outputModule\$($ModuleName)\Helpers") -Force
+Get-ChildItem -Path $RootDirectory -Recurse -Include "*.psd1","*.psm1" | Copy-Item -Destination (Join-Path $RootDirectory "outputModule\$($ModuleName)\") -Force
+
