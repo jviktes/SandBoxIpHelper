f9cde13a3fdf942e8c4591a1f742611701728e8e
pavel.vik@greycorbel.com <pavel.vik@greycorbel.com>
2024-12-16 10:33:02 +0100

    add module testing

---
+++ mainSandBoxIpHelper.Tests.ps1
@@ -0,0 +1,14 @@
+BeforeAll { 
+    Import-Module SandBoxIpHelper.IPAddressHelper
+}
+
+Describe "SandBoxIpHelper tests" {
+    Context "When an even number is inputted" {
+       It "Should return 2974023424"{
+            $ipAdressNumber = [GreyCorbel.Helpers.IPv4AddressHelper]::GetNumber("177.67.255.0")
+            $($ipAdressNumber)
+            "vysledky:"
+            $ipAdressNumber | Should -Be 2974023424
+        }
+    }
+}
