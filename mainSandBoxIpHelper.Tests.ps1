ac1e20ff1448bb50e1836c076e299792533a47cb
pavel.vik@greycorbel.com <pavel.vik@greycorbel.com>
2025-01-02 09:13:38 +0100

    code update

--- mainSandBoxIpHelper.Tests.ps1
+++ mainSandBoxIpHelper.Tests.ps1
@@ -9,7 +9,7 @@ Describe "SandBoxIpHelper tests" {
             $ipAdressNumber = [GreyCorbel.Helpers.IPv4AddressHelper]::GetNumber("177.67.255.0")
             $ipAdressNumber | Should -Be 2974023424
         }
-    },
+    }
     Context "When range 90.176.8.132/30 is inputted" {
         It "Should return 2974023424"{
             $bounds = [GreyCorbel.Helpers.IPv4AddressHelper]::GetRangeBoundaries("90.176.8.132/30")
@@ -17,8 +17,21 @@ Describe "SandBoxIpHelper tests" {
                 low  = $bounds.Item1
                 high = $bounds.Item2
             }
-            $bounds.low | Should -Be 2974023424
+            $bounds.low | Should -Be 1521485956
             $bounds.high | Should -Be 1521485959
+         }
+     }
+     Context "When range 90.176.8.132 is inputted" {
+        It "Should throw error"{
+            try {
+                $bounds =[GreyCorbel.Helpers.IPv4AddressHelper]::GetRangeBoundaries("90.176.8.132")
+                Should -Throw
+            }
+            catch {
+            # #     #Should -Be $null
+            }    
+            
+
          }
      }
 }
